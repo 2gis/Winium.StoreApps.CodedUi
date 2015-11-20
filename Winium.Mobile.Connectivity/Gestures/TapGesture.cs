05effbc38e917b5eb633eae713e64acafb286c35
Nick Abalov <n.abalov@2Gis.ru>
2015-11-20 11:08:39 +0600

    Copy Connectivity lib form Winium.StoreApps
    I know it is bad, but we are going to move into mono-repo soon, so we
    going take this debt of not using NuGet or submodules

---
+++ Winium.Mobile.Connectivity/Gestures/TapGesture.cs
@@ -0,0 +1,40 @@
+﻿namespace Winium.Mobile.Connectivity.Gestures
+{
+    #region
+
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+
+    #endregion
+
+    public class TapGesture : IGesture
+    {
+        #region Constructors and Destructors
+
+        public TapGesture(Point tapPoint, TimeSpan tapDuration = default(TimeSpan))
+        {
+            this.TapPosition = tapPoint;
+            this.PeriodBetweenPoints = tapDuration;
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        public TimeSpan PeriodBetweenPoints { get; set; }
+
+        public Point TapPosition { get; set; }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        public IEnumerable<Point> GetScreenPoints()
+        {
+            return new[] { this.TapPosition, this.TapPosition };
+        }
+
+        #endregion
+    }
+}
