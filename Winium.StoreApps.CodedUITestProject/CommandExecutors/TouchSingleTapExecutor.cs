34c031453c517dce00aadcd28f5cc0962f2ad412
Magnar Nedland <magnar.nedland@telenordigital.com>
2016-02-16 12:37:18 +0100

    Added TouchSingleTapExecutor.

---
+++ Winium.StoreApps.CodedUITestProject/CommandExecutors/TouchSingleTapExecutor.cs
@@ -0,0 +1,27 @@
+﻿namespace Winium.StoreApps.CodedUITestProject.CommandExecutors
+{
+    #region
+
+    using Winium.StoreApps.CodedUITestProject.Annotations;
+    using Winium.StoreApps.Common;
+
+    #endregion
+
+    [UsedImplicitly]
+    public class TouchSingleTapExecutor : CommandExecutorBase
+    {
+        #region Methods
+
+        protected override string DoImpl()
+        {
+            var registredId = this.ExecutedCommand.Parameters["element"].ToObject<string>();
+
+            var element = this.ElementsRegistry.GetRegistredElement(registredId);
+            element.Click();
+
+            return this.JsonResponse(ResponseStatus.Success, null);
+        }
+
+        #endregion
+    }
+}
